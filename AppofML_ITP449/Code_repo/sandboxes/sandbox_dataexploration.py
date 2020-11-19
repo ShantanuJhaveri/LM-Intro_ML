7896fe99835b2be24fe85365985fd0dbab531cf2
Shantanu Jhaveri <sj06434@usc.edu>
2020-11-19 15:37:57 -0800

    added sandboxes

---
+++ AppofML_ITP449/Code_repo/sandboxes/sandbox_dataexploration.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+data = {'ReleaseYear': [2009, 2019, 2015, 2013, 2005],
+        'Movie': ['Avatar', 'Avengers:Endgame', "Start Wars", 'Frozxen', 'HP andteh GObvlet of fire'],
+        'Expense': [32, 47, 38, 24, 20],
+        'Profit': [14, 10, 84, 79, 78]}
+
+df = pd.DataFrame(data)
+pd.set_option('display.max_columns', None)
+df['Ones'] = 1
+print(df)
