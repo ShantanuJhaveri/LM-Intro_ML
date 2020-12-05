50af8df45fe079b4aca1cabb3df48ca5279fbf96
Shantanu Jhaveri <sj06434@usc.edu>
2020-12-04 22:40:56 -0800

    FINAL PROJECT

--- AppofML_ITP449/Code_repo/finalproject_files/Jhaveri_Shantanu_FinalProject_Q3.py
+++ AppofML_ITP449/Code_repo/finalproject_files/Jhaveri_Shantanu_FinalProject_Q3.py
@@ -2,3 +2,65 @@
 # ITP 449 Fall 2020
 # Final Project
 # Q3
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import plot_confusion_matrix
+from sklearn import tree
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('/Users/shantanujhaveri/Desktop/work-git/GitHub/Learning-Modules_introML/AppofML_ITP449/Code_repo'
+                 '/finalproject_files/project_data/mushrooms.csv')
+
+# print(df.info())
+# print(df.isnull().sum())\
+
+X = df.drop(['class'], axis=1)
+y = df['class']
+X = pd.get_dummies(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=2020, stratify=y)
+
+dt = DecisionTreeClassifier(max_depth=6, random_state=2020)
+dt.fit(X_train, y_train)
+y_predict = dt.predict(X_test)
+y_predictTrain = dt.predict(X_train)
+
+# PART A
+plt.figure(1)
+plot_confusion_matrix(dt, X_test, y_test)  # Testing Data
+plt.figure(2)
+plot_confusion_matrix(dt, X_train, y_train)  # Training Data
+plt.show()
+
+# PART B/C
+print('\nACCURACY ON THE TRAIN PARTITION', accuracy_score(y_train, y_predictTrain))
+print('ACCURACY ON THE TEST PARTITION', accuracy_score(y_test, y_predict))
+
+# PART D
+plt.figure(2, figsize=(20, 10))
+fn = X.columns
+cn = list(map(str, dt.classes_.tolist()))
+irisTree = tree.plot_tree(dt, fontsize=12, filled=True, feature_names=fn, class_names=cn)
+plt.show()
+
+# PART E
+pd.set_option('display.max_rows', None)
+index_feature = 0
+index_column = 0
+# for i in dt.feature_importances_:
+#     print(index_feature, ' : ', i)
+#     index_feature += 1
+# for i in X.columns:
+#     print(index_column, ' : ', i)
+#     index_column += 1
+
+print('Feature Importance:', dt.feature_importances_)
+print('MOST IMPORTANT FEATURES: ODOR, STALK ROOT, STALK SURFACE ABOVE RING')
+# PART F
+print('PREDICTION FOR TEST CASE : ', dt.predict(
+    ['X', 'S', 'N', 'T', 'Y', 'G', 'C', 'N', 'K', 'E', 'E', 'S', 'S', 'W', 'W', 'P', 'W', 'O', 'P', 'R', 'S', 'U']))
--- AppofML_ITP449/Code_repo/finalproject_files/Jhaveri_Shantanu_FinalProject_Q4.py
+++ AppofML_ITP449/Code_repo/finalproject_files/Jhaveri_Shantanu_FinalProject_Q4.py
@@ -2,3 +2,70 @@
 # ITP 449 Fall 2020
 # Final Project
 # Q4
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.linear_model import Ridge
+from yellowbrick.regressor import ResidualsPlot
+
+df = pd.read_csv('/Users/shantanujhaveri/Desktop/work-git/GitHub/Learning-Modules_introML/AppofML_ITP449/Code_repo'
+                 '/finalproject_files/project_data/auto-mpg.csv')
+
+# PART A
+print('DATA FRAME SUMMARY:')
+print(df.describe())
+print('\nTHE MEAN MPG', df['mpg'].mean())
+
+# PART B
+print('THE MEDIAN MPG', df['mpg'].median())
+
+# PART C
+print('THE MEAN IS HIGHER THAN THE MEDIAN')
+# PLOT
+
+# PART D
+plt.figure(1)
+df_clean = df.drop(['No'], axis=1)
+sns.pairplot(df_clean)
+plt.show()
+
+# PART E
+print('THE STRONGEST CORRELATION IS BETWEEN MPG AND DISPLACEMENT')
+
+# PART F
+print('THE WEAKEST CORRELATION IS BETWEEN WEIGHT AND ACCELERATION')
+
+# PART G
+plt.figure(2)
+plt.scatter(df_clean['displacement'], df_clean['mpg'])
+plt.xlabel('Displacement')
+plt.ylabel('mpg')
+plt.show()
+
+# PART H
+plt.figure(3)
+model = LinearRegression(fit_intercept=True)
+X_train, X_test, y_train, y_test = train_test_split(df_clean.displacement, df_clean.mpg)
+LR = LinearRegression()
+LR.fit(X_train.values.reshape(-1, 1), y_train.values)
+prediction = LR.predict(X_test.values.reshape(-1, 1))
+plt.plot(X_test, prediction, label="Linear Regression", color='b')
+plt.scatter(X_test, y_test, label="Actual Testing Data", color='g')
+plt.legend()
+plt.show()
+print('INTERCEPT : ', LR.intercept_)
+print('COEFFICIENT : ', LR.coef_)
+print('EQUATION : [y = ', LR.intercept_, ' + ', LR.coef_, 'x]')
+print('PREDICTED MPG FOR DISPLACEMENT 220 : ', LR.predict(np.array([[220]]))[0])
+x = df_clean['displacement'].values
+# RESHAPING JUST INCASE THE ARRAY IS NOT A 2D ARRAY
+X = x.reshape(-1, 1)
+y = df_clean['mpg']
+ridge = Ridge()
+visualizer = ResidualsPlot(ridge)
+visualizer.fit(X, y)
+visualizer.show()
